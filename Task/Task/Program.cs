85bdc5553fb93a47bf6fc085424ce51d9cc3016f
SabirQ <sabirmg@code.edu.az>
2022-02-15 19:48:47 +0400

    Task_added

---
+++ Task/Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string sentence = "Everything I NeeD, is a CuP of CoFFee";
+
+
+            Console.WriteLine(Reverse(sentence));
+
+        }
+        public static string Reverse(string words)
+        {
+            char[] letters = words.ToCharArray();
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (char.IsUpper(letters[i]))
+                {
+                    letters[i]=char.ToLower(words[i]);
+                }
+                else
+                {
+                    letters[i] = char.ToUpper(words[i]);
+                }
+                words = new string(letters);
+            }
+            
+            return words;
+
+        }
+    }
+}
